8630fe5d7e2c2c60d33bc2d08c19aafaee2ff3bb
Justin Hernandez <jhernandezdev13@gmail.com>
2020-01-01 16:55:57 -0500

    Prepare Spring Environment
    Added JARs to classpath, created XML file for bean definitions, pseudocode, and logging support for spring info/errors.

---
+++ src/com/justin/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.justin.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		//Initialize spring container from XML
+		
+		//Create objects from XML bean definitions
+		
+		//Methods and output
+	}
+
+}
